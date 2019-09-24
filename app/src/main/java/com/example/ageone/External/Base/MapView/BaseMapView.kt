e9584ef79b7953566fcf067215d692a846a2347a
Katherina Yalanskaya <rinefica@gmail.com>
2019-09-24 12:01:41 +0300

    base progress bar, map

---
+++ app/src/main/java/com/example/ageone/External/Base/MapView/BaseMapView.kt
@@ -0,0 +1,8 @@
+package com.example.ageone.External.Base.MapView
+
+import com.example.ageone.Application.currentActivity
+import com.google.android.gms.maps.MapView
+
+class BaseMapView: MapView(currentActivity) {
+
+}
